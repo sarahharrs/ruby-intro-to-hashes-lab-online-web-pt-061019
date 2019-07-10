c7aa1b59f11b08d97f777e65070879ac99c20f2d
Sarah Harrs <sarahharrs@gmail.com>
2019-07-10 00:03:07 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,7 @@
 def new_hash
+  new_hash = {}
+  puts new_hash
+  
 
 end
 
@@ -7,7 +10,38 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 
 end
 
